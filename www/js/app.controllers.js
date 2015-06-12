36dfdf1143fb161c901a0e15d67865a19b3b90d3
methodcase <methodcase@gmail.com>
2015-06-12 21:25:52 +0800

    app controller

--- www/js/app.controllers.js
+++ www/js/app.controllers.js
@@ -12,10 +12,104 @@ angular.module('app.controllers',['ngMap'])
 	
 .controller('AddCtrl', function($scope, $http){
 
-	
+	$http.get('/rest/profile').
+	  success(function(data, status, headers, config) {     
+	  	$scope.profile = data;	  	
+	  	$scope.pic = "http://graph.facebook.com/"+data.id+"/picture?type=small";
+	  }).
+	  error(function(data, status, headers, config) {
+	});
+    
+    // Google Maps =======================================================================================
+    var map;
+    var markers = [];
+
+    initialize();
+    $scope.types = "['establishment']";
+
+    $scope.placeChanged = function() {
+      $scope.place = this.getPlace();
+      setLocation($scope.place);
+      console.log($scope.place.formatted_address)
+      console.log($scope.place.geometry.location.A)
+      console.log($scope.place.geometry.location.F)
+    };
+
+    function initialize() {
+      console.info('initialize');
+      var myLatlng = new google.maps.LatLng(10.302347504193767, 123.88662099838257);
+      var map = new google.maps.Map(document.getElementById('map-canvas'), {
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          center: myLatlng,
+          zoom: 15
+        });
+
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'lorem ipsum dolor sit amet consecteture, adispicing elit'
+      });
+    }	  
+
+    function setLocation(details) {
+      console.info(details);
+      var A = details.geometry.location.A;
+      var F = details.geometry.location.F;
+      var myLatlng = new google.maps.LatLng(A, F);
+      var map = new google.maps.Map(document.getElementById('map-canvas'), {
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          center: myLatlng,
+          zoom: 15
+      });
+
+      // This event listener will call addMarker() when the map is clicked.
+      google.maps.event.addListener(map, 'click', function(event) {
+          deleteMarkers();
+          addMarker(event.latLng, map, details.formatted_address);
+        });      
+      }
+
+      // Add a marker to the map and push to the array.
+      function addMarker(location, map, formatted_address) {
+        var marker = new google.maps.Marker({
+          position: location,
+          map: map,
+          title: formatted_address
+        });
+        console.info('location:', location)
+        markers.push(marker);
+      }
+
+      // Deletes all markers in the array by removing references to them.
+      function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+      }  
+
+      function clearMarkers() {
+        setAllMap(null);
+      }
+
+      // Sets the map on all markers in the array.
+      function setAllMap(map) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+        }
+      }
+    
 })
 
 .controller('ViewCtrl', function($scope, $http, $stateParams){
+	$scope.uni = $stateParams.id;
+	
+	$http.get('/rest/view/article/'+$scope.uni).
+	  success(function(data, status, headers, config) {     
+	  	$scope.article = data;
+	  	setLocation(data);
+	  }).
+	  error(function(data, status, headers, config) {
+	  });	
+
 	
 })
 
@@ -26,7 +120,7 @@ angular.module('app.controllers',['ngMap'])
 
 
 .controller('LogoutCtrl', function($scope, $http){
-	  
+		  
 })
 
 .directive('mapa', function(){	
--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,51 @@ angular.module('myApp', ['ui.router','app.controllers','app.service'])
 
 .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
   //================================================
+    // Check if the user is connected
+    //================================================
+    var checkLoggedin = function($q, $timeout, $http, $location, $rootScope){
+      // Initialize a new promise
+      var deferred = $q.defer();
+
+      // Make an AJAX call to check if the user is logged in
+      $http.get('/loggedin').success(function(user){
+        // Authenticated        
+        if (user !== '0')
+          /*$timeout(deferred.resolve, 0);*/
+          deferred.resolve();
+
+        // Not Authenticated
+        else {        	
+          
+          $rootScope.message = 'You need to log in.';
+          // $timeout(function(){deferred.reject();}, 0);
+          deferred.reject();
+          window.location.href="/#/app/login"
+        }
+      });
+      
+      return deferred.promise;
+    };
+    //================================================
     
+    //================================================
+    // Add an interceptor for AJAX errors
+    //================================================
+    $httpProvider.interceptors.push(function($q, $location) {
+      return {
+        response: function(response) {
+          // do something on success          
+          return response;
+        },
+        responseError: function(response) {
+          if (response.status === 401)
+            // $location.url('/login');
+        	window.location.href="/#/app/login"
+          return $q.reject(response);
+        }
+      };
+    });
+    //================================================
 
   // useStatesConfig($stateProvider);
 
