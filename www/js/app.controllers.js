d4eb979c3013770c44ca6783a3247a4275ef142d
methodcase <methodcase@gmail.com>
2015-06-12 21:22:47 +0800

    app controller

--- www/js/app.controllers.js
+++ www/js/app.controllers.js
@@ -1,1 +1,38 @@
-app.controllers.js
+angular.module('app.controllers',['ngMap'])
+
+.controller('HomeCtrl', function($scope, $http){
+
+	$http.get('/rest/article').
+	  success(function(data, status, headers, config) {     
+	  	$scope.articles = data;
+	  }).
+	  error(function(data, status, headers, config) {
+	 });			
+})
+	
+.controller('AddCtrl', function($scope, $http){
+
+	
+})
+
+.controller('ViewCtrl', function($scope, $http, $stateParams){
+	
+})
+
+.controller('LoginCtrl', function($scope, $http){
+	
+	  
+})
+
+
+.controller('LogoutCtrl', function($scope, $http){
+	  
+})
+
+.directive('mapa', function(){	
+
+	  
+})
+
+;
+
